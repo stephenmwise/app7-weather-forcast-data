25c29ef9e7de8566c15276bc644fe2c381d31f06
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-09-06 12:51:11 -0400

    Added streamlit and UI widgets

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.title("Weather Forcast For Upcoming Days")
+place = st.text_input("Place:")
+days = st.slider("Forcast Days", min_value=1, max_value=5,
+                 help="Select the number of forcasted days")
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
+
+st.subheader(f"{option} for the next {days} days in {place}")
